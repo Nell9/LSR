b69692a10533bbc6b1f306887b10d5806cd3d9c2
Рабочий пк <gmail.com>
2025-07-01 16:07:00 +0300

    Система с одной таблицей учета писем

--- LSR/doc/admin.py
+++ LSR/doc/admin.py
@@ -1,18 +1,99 @@
 from django.contrib import admin
-from .models import Letter
+from doc.models import Letters, Organizations, Disk, Type, Okr, Peoples
+from django.utils.html import format_html
+from django.urls import reverse
+from django.db.models import Q
+from django.utils.translation import gettext_lazy as _
+from rangefilter.filters import DateRangeFilter
+admin.site.site_header = "Учетик НИЛ-103"
+admin.site.site_title = "Панель управления"
+admin.site.index_title = "Добро пожаловать в админку"
 
 
-class PostAdmin(admin.ModelAdmin):
+@admin.register(Letters)
+class LettersAdmin(admin.ModelAdmin):
     list_display = (
-        'number',
-        'content',
-        'send_date'
-    )
-    list_editable = (
-        'content',
-    )
-    search_fields = ('number',)
-    list_filter = ('send_date',)
-    
+        "number",
+        "type",
+        "formated_addressees",
+        "okr",
+        "formated_content",
+        "send_date",
+        "formated_linked_disks",
+        "author",
 
-admin.site.register(Letter, PostAdmin)
+    )
+    search_fields = ("number", "content")
+    list_filter = (
+        "addressee",
+        "type",
+        "okr",
+        "author",
+        "workers",
+        ("send_date", DateRangeFilter),
+    )
+
+    @admin.display(description="Диски")
+    def formated_linked_disks(self, obj):
+        links = []
+        for disk in obj.disk.all():
+            url = reverse("admin:doc_disk_change", args=[disk.pk])
+            links.append(f'<a href="{url}">{disk.number}</a>')
+        return format_html("\n".join(links))
+
+    @admin.display(description="Адресаты")
+    def formated_addressees(self, obj):
+        return format_html("<br>".join([org.name for org in obj.addressee.all()]))
+
+    @admin.display(description="Содержание")
+    def formated_content(self, obj):
+        return (
+            (obj.content[:25] + obj.content[25:50] + "...")
+            if len(obj.content) > 50
+            else obj.content
+        )
+
+    def save_model(self, request, obj, form, change):
+        if not change or not obj.author:
+            obj.author = request.user
+        obj.save()
+
+
+@admin.register(Organizations)
+class OrganizationsAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+    list_filter = ("name",)
+
+
+@admin.register(Disk)
+class DiskAdmin(admin.ModelAdmin):
+    list_display = (
+        "number",
+        "copyes",
+        "content",
+    )
+
+    search_fields = ("number",)
+    list_filter = ("number",)
+
+
+@admin.register(Type)
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ("type",)
+    search_fields = ("type",)
+    list_filter = ("type",)
+
+
+@admin.register(Okr)
+class OkrAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+    list_filter = ("name",)
+
+
+@admin.register(Peoples)
+class PeoplesAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+    list_filter = ("name",)
--- LSR/doc/models.py
+++ LSR/doc/models.py
@@ -1,8 +1,6 @@
-
 from django.contrib.auth import get_user_model
-from django.urls import reverse
 from django.db import models
-
+from django.contrib.auth.models import User
 
 LENGHT_LIMIT = 20
 
@@ -10,72 +8,141 @@ User = get_user_model()
 
 
 class Type(models.Model):
-    type = models.CharField(
-        verbose_name='Статус отправки',
-        max_length=255
-    )
+    type = models.CharField(verbose_name="Статус отправки", max_length=255)
+
+    def __str__(self):
+        return self.type
+
+    class Meta:
+        verbose_name = "Тип документа"
+        verbose_name_plural = "Типы документов"
+        ordering = ["type"]
 
 
 class Organizations(models.Model):
-    name = models.TextField(
-        verbose_name="Название организации"
-    )
+    name = models.TextField(verbose_name="Название организации")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Организация"
+        verbose_name_plural = "Организации"
+        ordering = ["name"]
+
+
+class Okr(models.Model):
+    name = models.TextField(verbose_name="Название ОКР")
+
+    class Meta:
+        verbose_name = "Окр"
+        verbose_name_plural = "Окр"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
 
 
 class Disk(models.Model):
     number = models.CharField(
-        verbose_name='Номер диска',
-        max_length=255,
-        null=False
-    )
-    copyes = models.IntegerField(
-        verbose_name="Количество копий",
-        null=False
-    )
-    content = models.TextField(
-        verbose_name='Содержание',
-        null=False
-    )
+        verbose_name="Номер диска", max_length=255, null=False)
+    copyes = models.IntegerField(verbose_name="Количество копий", null=False)
+    content = models.TextField(verbose_name="Содержание", null=False)
+
+    def __str__(self):
+        return self.number
+
+    class Meta:
+        verbose_name = "Диск"
+        verbose_name_plural = "Диски"
+        ordering = ["number"]
 
 
-class Letter(models.Model):
+class Peoples(models.Model):
+    name = models.CharField(
+        max_length=255, verbose_name="Автор записи", auto_created=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Автор"
+        verbose_name_plural = "Авторы"
+        ordering = ["name"]
+
+
+class Letters(models.Model):
     number = models.CharField(
-        verbose_name='Номер письма',
-        max_length=255
+        verbose_name="Номер письма",
+        max_length=255,
+        null=False,
+        help_text="(обязательное)",
     )
     type = models.ForeignKey(
         Type,
         null=True,
-        verbose_name='Тип документа',
-        related_name='letters',
+        verbose_name="Тип документа",
+        related_name="letter",
         on_delete=models.PROTECT,
-        help_text='Акт, СЗ, Входящее...',
-        default=0
+        help_text="(обязательное)",
+        default=0,
     )
-    addressee = models.ForeignKey(
+    addressee = models.ManyToManyField(
         Organizations,
-        null=True,
-        on_delete=models.PROTECT,
-        verbose_name='Адресаты'
+        blank=True,
+        verbose_name="Адресаты",
+        help_text="(обязательное)",
     )
     content = models.TextField(
-        verbose_name='Содержание',
-        null=False
+        verbose_name="Содержание",
+        null=False,
+        help_text="(обязательное)",
     )
-    send_date = models.DateTimeField(
-        verbose_name='Дата отправки',
-        auto_now=True,
-        null=False
+    okr = models.ForeignKey(
+        Okr,
+        verbose_name="ОКР",
+        on_delete=models.PROTECT,
+        help_text="(не обязательное)",
+        default=None,
+        null=True,
+        blank=True,
     )
-    disk = models.OneToOneField(
+    send_date = models.DateField(
+        verbose_name="Дата отправки",
+        editable=True,
+        null=False,
+        help_text="(обязательное) Добавьте от какого числа письмо",
+    )
+    disk = models.ManyToManyField(
         Disk,
         verbose_name="Диск",
-        on_delete=models.CASCADE,
+        help_text=(
+            "(не обязательное) Добавьте новый диск для записи " "через LPBurn."
+        ),
+        blank=True,
+    )
+    author = models.CharField(
+        max_length=64,
+        verbose_name="Автор записи",
         null=True,
-        help_text="Добавьте диск для записи через LPBurn"
+        default="0",
+        editable=False,
+        help_text="Создается автоматически"
+    )
+    workers = models.ManyToManyField(
+        User,
+        verbose_name="Исполнители",
+        help_text="(не обязательное) Добавьте исполнителей письма",
+        blank=True,
     )
 
+    def __str__(self):
+        return self.number
+
     class Meta:
-        verbose_name = ('Письмо')
-        verbose_name_plural = ('Письма')
-        ordering = ['send_date']
+        verbose_name = "Письмо"
+        verbose_name_plural = "Письма"
+        ordering = ["send_date"]
+        constraints = [
+            models.UniqueConstraint(fields=['number', 'send_date'], name='unique_number_send_date')
+        ]
