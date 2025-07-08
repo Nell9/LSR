d4d8cc5372b7245d4a6a13ffd839545224fa3e57
Рабочий пк <gmail.com>
2025-07-08 17:15:37 +0300

    Добавлены связи между письмами добавлены стили

--- LSR/LSR/settings.py
+++ LSR/LSR/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -46,8 +46,8 @@ INSTALLED_APPS = [
     "doc",
     'rangefilter',
     "debug_toolbar",
+    'django_admin_filters',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
@@ -64,7 +64,7 @@ ROOT_URLCONF = "LSR.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -126,8 +126,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATIC_URL = '/static/'
 
-STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')  # путь к твоим кастомным static
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
--- LSR/doc/admin.py
+++ LSR/doc/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.utils.html import format_html
-from django.urls import reverse
 from rangefilter.filters import DateRangeFilter
-from doc.models import (
+from django import forms
+from .models import (
     topics,
     Organizations,
     Disk,
@@ -12,15 +11,16 @@ from doc.models import (
     IncomingLetter,
     AttachedFile,  # Letters
 )
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.utils.safestring import mark_safe
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.admin import GenericTabularInline
-User = get_user_model()
 
+User = get_user_model()
 admin.site.site_header = "Учетик НИЛ-103"
 admin.site.site_title = "Панель управления"
 admin.site.index_title = "Добро пожаловать в админку"
-
 # Общий миксин для Document-наследников, чтобы не дублировать код
 
 
@@ -40,6 +40,11 @@ class DocumentAdminMixin(admin.ModelAdmin):
         if not obj.author:
             obj.author = request.user
         super().save_model(request, obj, form, change)
+
+    def get_topics(self, obj):
+        return ", ".join([org.name for org in obj.topic.all()])
+
+    get_topics.short_description = "Темы"
 
 
 @admin.register(topics)
@@ -68,7 +73,7 @@ class MemoAdmin(DocumentAdminMixin):
     list_display = (
         "number",
         "self_date",
-        "type", 
+        "type",
         "content",
         "get_topics",
         "get_addressees",
@@ -80,11 +85,11 @@ class MemoAdmin(DocumentAdminMixin):
         "author",
         "topic",
         "type",
+        ("self_date", DateRangeFilter),
     )
     date_hierarchy = "self_date"
     inlines = [AttachedFileInline]
     search_fields = ("number", "content")
-
     fields = ("number", "self_date", "type", "content",
               "topic", "addressee", "author")
 
@@ -93,20 +98,21 @@ class MemoAdmin(DocumentAdminMixin):
 
     get_addressees.short_description = "Адресаты"
 
-    def get_topics(self, obj):
-        return ", ".join([org.name for org in obj.topic.all()])
-
-    get_topics.short_description = "Темы"
-
 
 @admin.register(Act)
 class ActAdmin(DocumentAdminMixin):
     list_display = ("number", "self_date", "content",
                     "stamp", "topic", "author")
-    list_filter = (("self_date", DateRangeFilter), "stamp", "author")
+    list_filter = (
+        "stamp",
+        "topic",
+        "author",
+        ("self_date", DateRangeFilter),
+    )
     date_hierarchy = "self_date"
     inlines = [AttachedFileInline]
     fields = ("number", "self_date", "type", "content", "topic", "author")
+    search_fields = ("number", "content")
 
 
 @admin.register(OutgoingLetter)
@@ -115,36 +121,48 @@ class OutgoingLetterAdmin(DocumentAdminMixin):
         "number",
         "self_date",
         "outgoing_date",
+        "get_topics",
         "content",
         "get_addressees",
         "author",
         "get_disks",
         "incoming_letters_list",
     )
-    filter_horizontal = ("addressee", "disk", "answers_to")
-    list_filter = ("addressee", ("outgoing_date", DateRangeFilter),
-                   "author", )  # Добавить сюда выпор письма для связи
+    filter_horizontal = ("addressee", "disk", "answers_to", "topic")
+    list_filter = (
+        "addressee",
+        "topic",
+        # Добавить сюда выпор письма для связи
+        "author",
+        ("outgoing_date", DateRangeFilter),
+        ("self_date", DateRangeFilter),
+    )
     date_hierarchy = "outgoing_date"
     inlines = [AttachedFileInline]
     fields = (
         "number",
         "self_date",
         "outgoing_date",
+        "topic",
         "content",
         "addressee",
         "answers_to",
         "disk",
     )
     readonly_fields = ("incoming_letters_list",)
+    search_fields = ("number", "content")
 
     def incoming_letters_list(self, obj):
         incoming_letters = obj.incoming_letters.all()
         if not incoming_letters:
             return "Нет ответов"
-        return mark_safe("<br>".join(
-            f"<a href='/doc/incomingletter/{i.pk}/change/'>{i}</a>"
-            for i in incoming_letters
-        ))
+        return mark_safe(
+            "<br>".join(
+                f"<a href='/doc/incomingletter/?number={i.number}' class='minimal-link'>"
+                f"<span class='icon'>📄</span>{i}</a>"
+                for i in incoming_letters
+            )
+        )
 
     incoming_letters_list.short_description = "вх. ответы:"
 
@@ -158,6 +176,23 @@ class OutgoingLetterAdmin(DocumentAdminMixin):
 
     get_disks.short_description = "Диски"
 
+    def changelist_view(self, request, extra_context=None):
+        # Контекст для кнопки
+        if extra_context is None:
+            extra_context = {}
+
+        # Построим URL фильтрации по текущему пользователю
+        filter_url = f"{reverse('admin:doc_outgoingletter_changelist')}?author__id__exact={request.user.id}"
+
+        extra_context['my_letters_filter_url'] = filter_url
+
+        return super().changelist_view(request, extra_context=extra_context)
+
+    class Media:
+        css = {
+            'all': ('admin/css/linkstyle.css',)
+        }
+
 
 @admin.register(IncomingLetter)
 class IncomingLetterAdmin(DocumentAdminMixin):
@@ -167,14 +202,22 @@ class IncomingLetterAdmin(DocumentAdminMixin):
         "sender_number",
         "info",
         "sender_date",
+        "get_topics",
         "content",
         "addressee",
         "get_workers",
         "author",
         "answer_by_list",
     )
-    list_filter = ("addressee", ("sender_date", DateRangeFilter))
-    filter_horizontal = ("workers", "answer_by")
+    list_filter = (
+        "addressee",
+        "topic",
+        ("sender_date", DateRangeFilter),
+        ("self_date", DateRangeFilter),
+        "author",
+        "workers",
+    )
+    filter_horizontal = ("workers", "answer_by", "topic")
     date_hierarchy = "sender_date"
     inlines = [AttachedFileInline]
     fields = (
@@ -183,12 +226,18 @@ class IncomingLetterAdmin(DocumentAdminMixin):
         "sender_number",
         "info",
         "sender_date",
+        "topic",
         "content",
         "addressee",
         "answer_by",
         "workers",
     )
-    search_fields = ("number", "content")
+    search_fields = (
+        "number",
+        "sender_number",
+        "content",
+        "info",
+    )
 
     readonly_fields = ("answer_by_list",)
 
@@ -204,12 +253,16 @@ class IncomingLetterAdmin(DocumentAdminMixin):
         # Письма, на которые это входящее отвечает
         for i in answer_by:
             links.append(
-                f"<a href='/admin/doc/outgoingletter/{i.pk}/change/'>{i}</a>")
+                f"<a href='/doc/outgoingletter/?number={i.number}' class='minimal-link'>"
+                f"<span class='icon'>📄</span>{i}</a>"
+            )
 
-        # Исходящие письма, которые ответили на это входящее
+    # Исходящие письма, которые ответили на это входящее
         for i in answered_by_outgoing:
             links.append(
-                f"<a href='/admin/doc/outgoingletter/{i.pk}/change/'>{i}</a>")
+                f"<a href='/doc/outgoingletter/?number={i.number}' class='minimal-link'>"
+                f"<span class='icon'>📄</span>{i}</a>"
+            )
 
         return mark_safe("<br>".join(links))
 
@@ -219,3 +272,20 @@ class IncomingLetterAdmin(DocumentAdminMixin):
         return ", ".join([worker.username for worker in obj.workers.all()])
 
     get_workers.short_description = "Исполнители"
+
+    def changelist_view(self, request, extra_context=None):
+        # Контекст для кнопки
+        if extra_context is None:
+            extra_context = {}
+
+        # Построим URL фильтрации по текущему пользователю
+        filter_url = f"{reverse('admin:doc_incomingletter_changelist')}?workers__id__exact={request.user.id}"
+
+        extra_context['my_letters_filter_url'] = filter_url
+
+        return super().changelist_view(request, extra_context=extra_context)
+
+    class Media:
+        css = {
+            'all': ('admin/css/linkstyle.css',)
+        }
